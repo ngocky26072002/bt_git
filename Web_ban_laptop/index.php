c832af82c72391a49c8fe3b4beb84a8ab76ec318
NgocKy <ngocky26072002@gmail.com>
2023-12-06 10:56:14 +0700

    them F

---
+++ Web_ban_laptop/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Web bán Laptop</title>
+</head>
+<body>
+    <div class="wraper">
+
+        <div class= "header">
+
+        </div>
+
+        <div class= "menu">
+            <ul class="list_menu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Danh mục sản phẩm</a></li>
+                <li><a href="#">Giỏ hàng</a></li>
+                <li><a href="#">Tin Tức</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+
+        <div id= "main">
+
+            <div class="sidebar">
+                <ul class="list_sidebar">
+                    <li><a href="#">Giá đỡ</a></li>
+                    <li><a href="#">Đế tản nhiệt</a></li>
+                    <li><a href="#">Bàn phím và chuột không dây</a></li>
+                    <li><a href="#">Lót chuột</a></li>
+                    <li><a href="#">Phụ kiện laptop khác</a></li>
+                </ul>
+            </div>
+
+            <div class="main_content">
+                <p>Main content</p>
+            </div>
+
+            <div class ="clear">
+
+            </div>
+        </div>
+
+        <div class= "footer">
+            <p>footer</p>
+        </div>
+
+    </div>
+</body>
+</html>
